0265f5edcfa21e5ff3db7b7642bbe55d31d78687
anubhab2001-stack <anubhabmukherjee2001@gmail.com>
2026-07-03 20:29:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,23 @@ function copyLink(url) {
     console.error("Failed to copy link: ", err);
   });
 }
+
 let dwValue = 0;
 function darkWhite(){
   const header = document.querySelector('header');
-  const footer = document.querySelector('footer');
-  const dwButton = document.querySelector('.dark-white-button');
-  const archiveDw = document.querySelectorAll('.archive-section h3 a')
-  console.log(archiveDw[0]);
+  const dwButton = document.querySelector('#dark-white');
   if (dwValue == 0){
     dwValue = 1;
     document.body.style.color = "white";
     document.body.style.backgroundColor = "black";
     header.style.background = "linear-gradient(120deg, lightgreen, black)";
-    footer.style.background = "linear-gradient(120deg, rgb(98, 255, 0), rgb(127, 234, 255))";
-    dwButton.src = "dark.svg";
-    for(let i = 0; i < archiveDw.length; i++){
-      archiveDw[i].style.color = "white";
-    }
+    dwButton.src = "white.svg";
   }
   else if (dwValue == 1){
     dwValue = 0;
     document.body.style.color = "black";
     document.body.style.backgroundColor = "white";
     header.style.background = "linear-gradient(120deg, red, red)";
-    footer.style.background = "linear-gradient(120deg, rgb(255, 85, 0), rgb(182, 127, 255)";
-    dwButton.src = "white.svg";
-    for(let i = 0; i <= archiveDw.length; i++){
-      archiveDw[i].style.color = "black";
-    }
+    dwButton.src = "dark.svg";
   }
 }
