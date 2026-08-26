22c237e6a5c5b6f401eb59e5b7fe3d2aa5db9023
anubhab2001-stack <anubhabmukherjee2001@gmail.com>
2026-08-26 20:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function copyLink(url) {
   });
 }
 
-let dwValue = 0;
+let dwValue = 1;
 function darkWhite(){
   const header = document.querySelector('header');
   const dwButton = document.querySelector('#dark-white');
@@ -14,14 +14,18 @@ function darkWhite(){
     dwValue = 1;
     document.body.style.color = "white";
     document.body.style.backgroundColor = "black";
-    header.style.background = "linear-gradient(120deg, red, black)";
+    header.style.background = "linear-gradient(rgba(25, 25, 112, 0.2), rgba(0, 0, 0, 0.5)), url('Ross.jpeg')";
+    header.style.backgroundPosition ='center';
+    header.style.backgroundSize = 'cover';
     dwButton.src = "white.svg";
   }
   else if (dwValue == 1){
     dwValue = 0;
     document.body.style.color = "black";
     document.body.style.backgroundColor = "white";
-    header.style.background = "linear-gradient(120deg, red, red)";
+    header.style.background = "linear-gradient(rgba(25, 25, 112, 0.1), rgba(25, 25, 112, 0.1)), url('Ross.jpeg')";
+    header.style.backgroundPosition ='center';
+    header.style.backgroundSize = 'cover';
     dwButton.src = "dark.svg";
   }
 }
